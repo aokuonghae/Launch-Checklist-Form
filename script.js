98eeed31b2d1de86489bd008cf40ba60b37ee222
Anastasia Okuonghae <aokuonghae@gmail.com>
2019-12-04 22:23:28 -0600

    Finished Assignment

--- script.js
+++ script.js
@@ -1,13 +1,64 @@
-// Write your JavaScript code here!
+window.addEventListener("load", function() {
+   const form = document.getElementById("launchForm");
+   const flight = document.getElementById("faultyItems");
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+         const destination = document.getElementById("missionTarget");
+         let index=Math.floor(Math.random() * json.length);
+         destination.innerHTML = `
+            <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[index].name}</li>
+               <li>Diameter: ${json[index].diameter}</li>
+               <li>Star: ${json[index].star}</li>
+               <li>Distance from Earth: ${json[index].distance}</li>
+               <li>Number of Moons: ${json[index].moons}</li>
+            </ol>
+            <img src="${json[index].image}"></img>
+            `
+      });
+   });
+
+      window.addEventListener("submit", function(evt) {
+         evt.preventDefault()
+         let mainPilot = document.querySelector("input[name=pilotName]").value;
+         let coPilot = document.querySelector("input[name=copilotName]").value;
+         let fuel = document.querySelector("input[name=fuelLevel]").value;
+         let massCargo = document.querySelector("input[name=cargoMass]").value;
+         let houstonWeAreGo = document.getElementById("launchStatus");
+         let fuelGo = true;
+         let cargoGo = true;
+
+         if (mainPilot === "" || coPilot === "" || fuel === "" || massCargo === "") {
+            alert("All fields are required!");
+            event.preventDefault();
+         } else if (/^[a-zA-Z/s]+$/.test(mainPilot) === false || /^[a-zA-Z ]+$/.test(coPilot) === false) {
+            alert("Please enter a valid name");
+            event.preventDefault();
+         } else if (/^[0-9/.]+$/.test(fuel) === false || /^[0-9]+$/.test(massCargo) === false) {
+            alert("Please enter a valid number");
+            event.preventDefault();
+         } else {
+            document.getElementById("pilotStatus").innerHTML = `${mainPilot} is ready for launch`;
+            document.getElementById("copilotStatus").innerHTML = `${coPilot} is ready for launch.`;
+            if (fuel < 10000) {
+               fuelGo = false;
+               document.getElementById("fuelStatus").innerHTML = `Fuel level not enough for launch.`;
+            }
+            if (massCargo > 10000) {
+               cargoGo = false;
+               document.getElementById("cargoStatus").innerHTML = `Too much mass for launch.`;
+            }
+            if (fuelGo === false || cargoGo === false) {
+               flight.style.visibility = "visible";
+               houstonWeAreGo.style.color = "red";
+               houstonWeAreGo.innerHTML = `Shuttle not ready for launch`;
+            } else {
+               flight.style.visibility = "hidden";
+               houstonWeAreGo.style.color = "green";
+               houstonWeAreGo.innerHTML = `Shuttle is ready for launch`;
+            }
+         }  
+      });
+   });
